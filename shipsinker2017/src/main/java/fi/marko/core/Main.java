f18da8111b8d2ed036fc243921b2a14b42bde750
markokoskinen2037 <markokoskinen2037@gmail.com>
2017-01-20 19:01:05 +0200

    pakettien nimet muutettu loogisiksi

--- shipsinker2017/src/main/java/fi/marko/core/Main.java
+++ shipsinker2017/src/main/java/fi/marko/core/Main.java
@@ -1,11 +1,12 @@
-package core;
+package fi.marko.core;
+
+import fi.marko.gui.GUI;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Beast
